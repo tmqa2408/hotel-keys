321b0c9d995bca47e56d759ab69e38f970793097
tmqa2408 <tmqa2408@gmail.com>
2025-12-17 07:52:20 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+let guests = JSON.parse(localStorage.getItem("guests")) || [];
+
+const list = document.getElementById("guestList");
+const search = document.getElementById("search");
+
+function render() {
+    list.innerHTML = "";
+
+    const q = search.value.toLowerCase();
+
+    guests
+        .filter(g => g.name.toLowerCase().includes(q))
+        .forEach((g, i) => {
+            const li = document.createElement("li");
+            li.innerHTML = `
+                <span>${g.name}</span>
+                <strong>${g.room}</strong>
+            `;
+            li.onclick = () => {
+                if (confirm("Удалить постояльца?")) {
+                    guests.splice(i, 1);
+                    save();
+                }
+            };
+            list.appendChild(li);
+        });
+}
+
+function save() {
+    localStorage.setItem("guests", JSON.stringify(guests));
+    render();
+}
+
+document.getElementById("addBtn").onclick = () => {
+    const name = prompt("Имя и фамилия");
+    const room = prompt("Номер комнаты");
+
+    if (name && room) {
+        guests.push({ name, room });
+        save();
+    }
+};
+
+search.oninput = render;
+
+render();
